13cb3608603551b951079ab7f5b5cf11803d0441
bhrgv <bhargav2014duggu@gmail.com>
2015-11-29 14:07:11 +0530

    Submit Commit

--- src/012sllSort.cpp
+++ src/012sllSort.cpp
@@ -22,5 +22,40 @@ struct node {
 };
 
 void sll_012_sort(struct node *head){
-	
+	struct node* curr = head;
+	int c0 = 0, c1 = 0, c2 = 0;
+	while (curr != NULL)
+	{
+		if (curr->data == 0)
+			c0++;
+		else if (curr->data == 1)
+			c1++;
+		else if (curr->data == 2)
+			c2++;
+		curr = curr->next;
+	}
+	int i = 0, j = 0, k = 0,index=0;
+	curr = head;
+	for (i = 0; i < 3; i++)
+	{
+		switch(i){
+		case 0:
+			j = 0;
+			k = c0;
+			break;
+		case 1:
+			j = 1;
+			k = c1;
+			break;
+		case 2:
+			j = 2;
+			k = c2;
+			break;
+		}
+		for (index = 0; index < k; index++)
+		{
+			curr->data = j;
+			curr = curr->next;
+		}
+	}
 }
