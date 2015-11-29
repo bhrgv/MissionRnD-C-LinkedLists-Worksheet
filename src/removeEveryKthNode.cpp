e7afcbb7593b4e5093cf267e596d3150c3e144ad
bhrgv <bhargav2014duggu@gmail.com>
2015-11-29 14:37:34 +0530

    Submit Commit

--- src/removeEveryKthNode.cpp
+++ src/removeEveryKthNode.cpp
@@ -12,12 +12,29 @@ NOTES:
 */
 
 #include <stdio.h>
-
 struct node {
 	int num;
 	struct node *next;
 };
 
 struct node * removeEveryKthNode(struct node *head, int K) {
-	return NULL;
+	if (K==1||K<0||head==NULL||K==0)
+		return NULL;
+	int i = 0;
+	int j = K - 1;
+	struct node start;
+	start.next = head;
+	struct node* curr = &start;
+	while (curr->next != NULL)
+	{
+		if (i == j)
+		{
+			curr -> next = curr->next->next;
+			i = (i + 1) % K;
+		}
+		i = (i + 1) % K;
+		if (curr->next!=NULL)
+		curr = curr->next;
+	}
+	return head;
 }
